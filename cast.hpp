f2af121d090f99bdfb5211e6ec3bb3baaaa0ff70
Yang Donglin <332407470@qq.com>
2019-01-07 10:35:18 +0800

    new cast header

---
+++ cast.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <type_traits>
+#include <gsl/span>
+
+
+
+namespace cc
+{
+
+using std::underlying_type;
+using std::is_pod;
+using gsl::span;
+
+template <typename Enum>
+inline auto enum_cast (Enum e) -> typename underlying_type<Enum>::type
+{
+	return static_cast<typename underlying_type<Enum>::type>(e);
+}
+
+template <typename Pod>
+inline span<char> byte_cast (Pod& pod)
+{
+	static_assert (is_pod<Pod>::value, "byte_cast cannot be applied to non-pod values");
+	return span<char> (reinterpret_cast<char*>(&pod), sizeof (pod));
+}
+
+
+
+}
--- file.hpp
+++ file.hpp
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include <memory>
 #include <gsl/pointers>
+#include <cc/null.hpp>
 #include <optional>
 #include <cc/span.hpp>
+#include <cc/cast.hpp>
+#include <cc/scope.hpp>
 #include <gsl/pointers>
 
 
@@ -16,135 +19,135 @@ using gsl::not_null;
 
 enum class FilePos : int
 {
-    Head = SEEK_SET,
-    Tail = SEEK_END,
-    Current = SEEK_CUR,
+	Head = SEEK_SET,
+	Tail = SEEK_END,
+	Current = SEEK_CUR,
 };
 
 class file final
 {
-    template<typename T>
-    using basic_string_view = std::basic_string_view<T>;
-    using string_view = std::string_view;
+	template<typename T>
+	using basic_string_view = std::basic_string_view<T>;
+	using string_view = std::string_view;
 
 public:
-    static file open (not_null<const char*> file_name, not_null<const char*> mode) noexcept
-    {
-        return ::fopen (file_name, mode);
-    }
+	static file open (not_null<const char*> file_name, not_null<const char*> mode) noexcept
+	{
+		return ::fopen (file_name, mode);
+	}
 
-    FILE* get () noexcept { return fp_.get (); }
-    auto ftell () noexcept { return ::ftell (get ()); }
+	FILE* get () noexcept { return fp_.get (); }
+	auto ftell () noexcept { return ::ftell (get ()); }
 
-    int fseek (FilePos origin, long offset = 0) noexcept
-    {
-        return ::fseek (get (), offset, static_cast<int>(origin));
-    }
+	int fseek (FilePos origin, long offset = 0) noexcept
+	{
+		return ::fseek (get (), offset, static_cast<int>(origin));
+	}
 
-    size_t fwrite (span<const char> view) noexcept
-    {
-        return ::fwrite (view.data (), sizeof (char), static_cast<size_t> (view.size()), get ());
-    }
+	size_t fwrite (span<const char> view) noexcept
+	{
+		return ::fwrite (view.data (), sizeof (char), static_cast<size_t> (view.size()), get ());
+	}
 
-    auto fread (span<char> view) noexcept
-    {
-        return ::fread (view.data (), sizeof (char), static_cast<size_t> (view.size ()), get ());
-    }
+	auto fread (span<char> view) noexcept
+	{
+		return ::fread (view.data (), sizeof (char), static_cast<size_t> (view.size ()), get ());
+	}
 
-    template<typename ... Args>
-    int fprintf (const char * fmt, Args && ... args) noexcept
-    {
-        return ::fprintf (get (), fmt,  std::forward<Args> (args)...);
-    }
+	template<typename ... Args>
+	int fprintf (const char * fmt, Args && ... args) noexcept
+	{
+		return ::fprintf (get (), fmt,  std::forward<Args> (args)...);
+	}
 
-    explicit operator bool () const noexcept { return fp_ != nullptr; }
+	explicit operator bool () const noexcept { return fp_ != nullptr; }
 
-    [[nodiscard]]
-    inline static std::optional<std::string> read_all (not_null<const char*> path) noexcept;
+	[[nodiscard]]
+	inline static std::optional<std::string> read_all (not_null<const char*> path) noexcept;
 
-    inline static bool override (not_null<const char*> path, string_view data) noexcept;
-    inline static bool append (not_null<const char*> path, string_view data) noexcept;
+	inline static bool override (not_null<const char*> path, gsl::span<const char> data) noexcept;
+	inline static bool append (not_null<const char*> path, string_view data) noexcept;
 
 private:
-    file (FILE * fp) noexcept : fp_ (fp) { }
+	file (FILE * fp) noexcept : fp_ (fp) { }
 private:
-    struct fclose_deleter { void operator () (FILE* fp) { fclose (fp); } };
-    std::unique_ptr<FILE, fclose_deleter> fp_ = nullptr;
+	struct fclose_deleter { void operator () (FILE* fp) { fclose (fp); } };
+	std::unique_ptr<FILE, fclose_deleter> fp_ = nullptr;
 };
 
 
 [[nodiscard]]
 std::optional<std::string> file::read_all (not_null<const char*> path) noexcept
 {
-    auto fp = file::open (path, not_null("rb"));
-    if (!fp)
-    {
-        return {};
-    }
+	auto fp = file::open (path, not_null("rb"));
+	if (!fp)
+	{
+		return {};
+	}
 
-    if (fp.fseek (FilePos::Tail) != 0)
-    {
-        return {};
-    }
+	if (fp.fseek (FilePos::Tail) != 0)
+	{
+		return {};
+	}
 
-    const auto len = fp.ftell ();
+	const auto len = fp.ftell ();
 
-    if (len == -1L)
-    {
-        return {};
-    }
+	if (len == -1L)
+	{
+		return {};
+	}
 
-    const auto u_len = static_cast<size_t> (len);
+	const auto u_len = static_cast<size_t> (len);
 
-    if (fp.fseek (FilePos::Head) != 0)
-    {
-        return {};
-    }
+	if (fp.fseek (FilePos::Head) != 0)
+	{
+		return {};
+	}
 
-    std::string ret;
-    ret.resize (u_len);
+	std::string ret;
+	ret.resize (u_len);
 
-    if (fp.fread (ret) != u_len)
-    {
-        return std::nullopt;
-    }
+	if (fp.fread (ret) != u_len)
+	{
+		return std::nullopt;
+	}
 
-    return std::move (ret);
+	return std::move (ret);
 }
 
-bool file::override(not_null<const char *> path, string_view data) noexcept
+bool file::override(not_null<const char *> path, gsl::span<const char> data) noexcept
 {
-    auto fp = file::open (path, not_null("wb"));
-    if (!fp)
-    {
-        return false;
-    }
-    const auto len = fp.fwrite (data);
+	auto fp = file::open (path, not_null("wb"));
+	if (!fp)
+	{
+		return false;
+	}
+	const auto len = fp.fwrite (data);
 
-    if (len != data.size ())
-    {
-        return false;
-    }
+	if (len != static_cast<size_t> (data.size ()))
+	{
+		return false;
+	}
 
-    return true;
+	return true;
 }
 
 [[nodiscard]]
 bool file::append(not_null<const char *> path, string_view data) noexcept
 {
-    auto fp = file::open (path, not_null("ab+"));
-    if (!fp)
-    {
-        return false;
-    }
-    const auto len = fp.fwrite (data);
+	auto fp = file::open (path, not_null("ab+"));
+	if (!fp)
+	{
+		return false;
+	}
+	const auto len = fp.fwrite (data);
 
-    if (len != data.size ())
-    {
-        return false;
-    }
+	if (len != data.size ())
+	{
+		return false;
+	}
 
-    return true;
+	return true;
 }
 
 }
