48e6c7620a382465068896c9375ce645e6be9ed9
Yang Donglin <332407470@qq.com>
2019-01-09 11:59:58 +0800

    const version of byte_cast

--- cast.hpp
+++ cast.hpp
@@ -18,12 +18,18 @@ inline auto enum_cast (Enum e) -> typename underlying_type<Enum>::type
 }
 
 template <typename Pod>
-inline span<char> byte_cast (Pod& pod)
+inline span<char> byte_cast (Pod & pod)
 {
 	static_assert (is_pod<Pod>::value, "byte_cast cannot be applied to non-pod values");
 	return span<char> (reinterpret_cast<char*>(&pod), sizeof (pod));
 }
 
+template <typename Pod>
+inline span<const char> byte_cast (const Pod & pod)
+{
+	static_assert (is_pod<Pod>::value, "byte_cast cannot be applied to non-pod values");
+	return span<const char> (reinterpret_cast<const char*>(&pod), sizeof (pod));
+}
 
 
 }
